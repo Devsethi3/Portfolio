1127d057d2059367f4343fcefb7d396eda2a355d
Devsethi3 <devsethi4522@gmail.com>
2024-06-10 19:49:51 +0530

    Update Project.tsx

--- src/data/index.ts
+++ src/data/index.ts
@@ -42,7 +42,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently building a animated UI Library",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
